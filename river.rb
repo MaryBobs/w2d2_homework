7164634c5f8b4c8bc0e86c6801d26ed01840e4f2
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2019-10-22 18:22:08 +0100

    added river and test for name

---
+++ river.rb
@@ -0,0 +1,24 @@
+class River
+
+attr_reader :name
+
+def initialize(name)
+  @name = name
+  @river_stock = []
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -4,6 +4,15 @@ require_relative("../river.rb")
 
 class TestRiver < MiniTest::Test
 
+def setup
+
+  @river1 = River.new("Mersey")
+
+end
+
+def test_if_river_has_name
+  assert_equal("Mersey", @river1.name)
+end
 
 
 
