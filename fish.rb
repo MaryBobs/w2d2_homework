712159397b7b6dab9486f1bbcffbcb161e60e7f2
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2019-10-22 18:17:04 +0100

    created some fish and tested name

--- fish.rb
+++ fish.rb
@@ -1,6 +1,6 @@
 class Fish
 
-attr_reader
+attr_reader :name
 
 def initialize(name)
   @name = name
--- specs/fish_spec.rb
+++ specs/fish_spec.rb
@@ -4,7 +4,19 @@ require_relative("../fish.rb")
 
 class TestFish < MiniTest::Test
 
+  def setup
 
+  @fish1 = Fish.new("Bill")
+  @fish2 = Fish.new("Fred")
+  @fish3 = Fish.new("Dave")
+  @fish4 = Fish.new("Jeff")
+  @fish5 = Fish.new("Pete")
+
+  end
+
+def test_if_fish_has_name
+  assert_equal("Fred", @fish2.name)
+end
 
 
 
