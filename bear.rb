bf319b3587676d30e5ea64bc366aa3b522a2af11
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2019-10-22 18:29:04 +0100

    created a bear and tests for name and type

---
+++ bear.rb
@@ -0,0 +1,22 @@
+class Bear
+
+attr_reader :name, :type
+
+def initialize(name,type)
+  @name = name
+  @type = type
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -4,7 +4,19 @@ require_relative("../bear.rb")
 
 class TestBear < MiniTest::Test
 
+def setup
 
+  @bear1 = Bear.new("Alfred", "Great Bear")
+
+end
+
+def test_bear_has_name
+  assert_equal("Alfred", @bear1.name)
+end
+
+def test_bear_has_type
+  assert_equal("Great Bear", @bear1.type)
+end
 
 
 
